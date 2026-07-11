bd1b4014749e93709e7c67c822f05ec1b5c2e1ac
marg4ryn <poweski.jan@gmail.com>
2026-07-12 01:06:53 +0200

    feat: add store service

--- src/app/features/analysis-run/data-access/store/store.service.spec.ts
+++ src/app/features/analysis-run/data-access/store/store.service.spec.ts
@@ -1,16 +1,39 @@
 import { TestBed } from '@angular/core/testing';
 
 import { StoreService } from './store.service';
+import { LoggerService } from '@app/core/logging/logger.service';
 
 describe('StoreService', () => {
-  let service: StoreService;
+  let store: StoreService;
+  let logger: LoggerService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(StoreService);
+    store = TestBed.inject(StoreService);
+    logger = TestBed.inject(LoggerService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('resetState', () => {
+    it('should reset state', () => {
+      store.showModal.set(true);
+      store.isBusy.set(true);
+      store.progress.set('foo');
+      store.result.set('bar');
+      store.error.set('baz');
+
+      store.resetState();
+
+      expect(store.showModal()).toBeFalsy();
+      expect(store.isBusy()).toBeFalsy();
+      expect(store.progress()).toBe('');
+      expect(store.result()).toBe('');
+      expect(store.error()).toBe('');
+    });
+
+    it('should log on state reset', () => {
+      const infoSpy = vi.spyOn(logger, 'info');
+      store.resetState();
+      expect(infoSpy).toHaveBeenCalled();
+    });
   });
 });
--- src/app/features/analysis-run/data-access/store/store.service.ts
+++ src/app/features/analysis-run/data-access/store/store.service.ts
@@ -1,4 +1,22 @@
-import { Service } from '@angular/core';
+import { inject, Service, signal } from '@angular/core';
+import { LoggerService } from '@app/core/logging/logger.service';
 
 @Service()
-export class StoreService {}
+export class StoreService {
+  private readonly logger = inject(LoggerService);
+
+  showModal = signal<boolean>(false);
+  isBusy = signal<boolean>(false);
+  progress = signal<string>('');
+  result = signal<string>('');
+  error = signal<string>('');
+
+  resetState(): void {
+    this.showModal.set(false);
+    this.isBusy.set(false);
+    this.progress.set('');
+    this.result.set('');
+    this.error.set('');
+    this.logger.info('Store Service reset state');
+  }
+}
