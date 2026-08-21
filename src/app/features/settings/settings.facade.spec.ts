a529c16a5cd40474b25ad9f4e99a9ea9203cc3fe
marg4ryn <poweski.jan@gmail.com>
2026-08-21 17:36:07 +0200

    tests: add settings facade tests

--- src/app/features/settings/settings.facade.spec.ts
+++ src/app/features/settings/settings.facade.spec.ts
@@ -1,16 +1,56 @@
 import { TestBed } from '@angular/core/testing';
+import { signal } from '@angular/core';
 
 import { SettingsFacade } from './settings.facade';
+import { SettingsStoreService } from './data-access/settings-store/settings-store.service';
+import { SettingsService } from './data-access/service/settings.service';
 
 describe('SettingsFacade', () => {
   let service: SettingsFacade;
+  let store: {
+    showPanel: ReturnType<typeof signal<boolean>>;
+  };
+
+  let settingsService: {
+    openPanel: ReturnType<typeof vi.fn>;
+    closePanel: ReturnType<typeof vi.fn>;
+  };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    store = {
+      showPanel: signal(false),
+    };
+
+    settingsService = {
+      openPanel: vi.fn(),
+      closePanel: vi.fn(),
+    };
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: SettingsService, useValue: settingsService },
+        { provide: SettingsStoreService, useValue: store },
+      ],
+    });
     service = TestBed.inject(SettingsFacade);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('updates computed signals', () => {
+    store.showPanel.set(true);
+    expect(service.showPanel()).toBeTruthy();
+  });
+
+  it('handles openPanel', () => {
+    service.openPanel();
+    expect(settingsService.openPanel).toHaveBeenCalledOnce();
+  });
+
+  it('handles closePanel', () => {
+    service.closePanel();
+    expect(settingsService.closePanel).toHaveBeenCalledOnce();
   });
 });
