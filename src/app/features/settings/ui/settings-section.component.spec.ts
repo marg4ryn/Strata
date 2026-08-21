5b597c9b4144bf8f855a5f8ff43f8458d3b53b2f
marg4ryn <poweski.jan@gmail.com>
2026-08-21 18:15:54 +0200

    tests: add settings section component tests

--- src/app/features/settings/ui/settings-section.component.spec.ts
+++ src/app/features/settings/ui/settings-section.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SettingsSectionComponent } from './settings-section.component';
 
-describe.skip('SettingsSectionComponent', () => {
+describe('SettingsSectionComponent', () => {
   let component: SettingsSectionComponent;
   let fixture: ComponentFixture<SettingsSectionComponent>;
 
@@ -13,10 +13,15 @@ describe.skip('SettingsSectionComponent', () => {
 
     fixture = TestBed.createComponent(SettingsSectionComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('label', 'test');
+    fixture.detectChanges();
     await fixture.whenStable();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('sets label via input', () => {
+    const label = 'test label';
+    fixture.componentRef.setInput('label', label);
+    fixture.detectChanges();
+    expect(component.label()).toBe(label);
   });
 });
