7daa5cecebd4e6e2cde29161648c8c7ff453228a
marg4ryn <poweski.jan@gmail.com>
2026-08-21 15:34:42 +0200

    feat: add settings panel component

---
+++ src/app/features/settings/feature/settings-panel/settings-panel.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { A11yModule } from '@angular/cdk/a11y';
+
+import { SettingsFacade } from '../../settings.facade';
+import { SettingsSectionComponent } from '../../ui/settings-section.component';
+import { LanguageSwitcherComponent } from '../language-switcher/language-switcher.component';
+
+@Component({
+  selector: 'app-settings-panel',
+  imports: [A11yModule, SettingsSectionComponent, LanguageSwitcherComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './settings-panel.component.html',
+  styleUrl: './settings-panel.component.scss',
+})
+export class SettingsPanel {
+  protected readonly facade = inject(SettingsFacade);
+}
