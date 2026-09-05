73aa693abfe965e7ce699c3f34782fd13035720b
marg4ryn <poweski.jan@gmail.com>
2026-09-05 15:58:01 +0200

    feat: add repository details component proto

--- src/app/features/analysis-results/feature/repository-details/repository-details.component.ts
+++ src/app/features/analysis-results/feature/repository-details/repository-details.component.ts
@@ -1,18 +1,31 @@
 import { Component, inject, input } from '@angular/core';
+import { TranslocoPipe } from '@ngneat/transloco';
 
+import { LocalizedDurationPipe } from '@app/shared/localized-duration-pipe/localized-duration.pipe';
+import { LocalizedNumberPipe } from '@app/shared/localized-number-pipe/localized-number.pipe';
+import { LocalizedDatePipe } from '@app/shared/localized-date-pipe/localized-date.pipe';
 import { ResourcePageComponent } from '../resource-page/resource-page.component';
 import { pageResource } from '../../utils/page-resource';
 import { AnalysisResultsFacade } from '../../analysis-results.facade';
+import { DoughnutChartComponent } from '../../ui/doughnut-chart/doughnut-chart.component';
 
 @Component({
   selector: 'app-repository-details',
-  imports: [ResourcePageComponent],
+  imports: [
+    ResourcePageComponent,
+    TranslocoPipe,
+    LocalizedDatePipe,
+    LocalizedNumberPipe,
+    LocalizedDurationPipe,
+    DoughnutChartComponent,
+  ],
   templateUrl: './repository-details.component.html',
   styleUrl: './repository-details.component.scss',
 })
 export class RepositoryDetailsComponent {
-  id = input.required<string>();
   private readonly facade = inject(AnalysisResultsFacade);
+
+  id = input.required<string>();
 
   resource = pageResource(
     () => this.facade.getRepositorySummary(this.id()),
